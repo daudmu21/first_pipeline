70557ba7440264d869aa6ad0152ca5cb6a69ae43
daudmu21 <daudmuhudin@gmail.com>
2019-05-02 20:37:29 -0500

    change ENV

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,15 +4,15 @@ node{
         git 'git@github.com:daudmu21/cool_website.git'
     }
     stage("Webserver Install"){
-        sh "ssh ec2-user@54.171.239.132  sudo yum install httpd -y"
+        sh "ssh  ec2-user@${ENV}  sudo yum install httpd -y"
     }
     stage("Index file"){
-        sh "scp index.html  ec2-user@54.171.239.132:/tmp"
+        sh "scp index.html  ssh  ec2-user@${ENV}:/tmp"
     }
     stage("Move Index"){
-        sh 'ssh ec2-user@54.171.239.132   "sudo mv /tmp/index.html /var/www/html/index.html"'
+        sh 'ssh  ec2-user@${ENV}   "sudo mv /tmp/index.html /var/www/html/index.html"'
     }
     stage("Start HTTPD"){
-        sh "ssh ec2-user@54.171.239.132   sudo systemctl restart httpd"
+        sh ssh  "ec2-user@${ENV}   sudo systemctl restart httpd"
     }
 }
