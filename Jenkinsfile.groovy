18d1cf2df81c20cab3d070a8286b45ed35fb731b
daudmu21 <daudmuhudin@gmail.com>
2019-05-02 20:48:43 -0500

    add env

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,24 @@
 node{
-    properties([parameters([string(defaultValue: 'IP', description: 'Where\'s should I build', name: 'Environment ', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
+   properties([parameters([string(defaultValue: 'IP', description: 'where should I build?', name: 'ENV', trim: false)]), pipelineTriggers([pollSCM('* * * * *')])])
+   
+   
+   
     stage("Pull Repo"){
-        git 'git@github.com:daudmu21/cool_website.git'
+        git 'git git@github.com:daudmu21/cool_website.git'
+       
     }
-    stage("Webserver Install"){
-        sh "ssh  ec2-user@${ENV}  sudo yum install httpd -y"
+    stage("webserver Install"){
+        sh "ssh ec2-user@${ENV}      sudo yum install httpd -y"
     }
     stage("Index file"){
-        sh "scp index.html  ssh  ec2-user@${ENV}:/tmp"
+        sh "scp index.html                    ec2-user@${ENV}:/tmp"
+
     }
-    stage("Move Index"){
-        sh 'ssh  ec2-user@${ENV}   "sudo mv /tmp/index.html /var/www/html/index.html"'
+    stage("move Index"){
+        sh "ssh ec2-user@${ENV}    sudo mv /tmp/index.html /var/www/html/index.html"
     }
-    stage("Start HTTPD"){
-        sh "ssh  ec2-user@${ENV}   sudo systemctl restart httpd"
+    stage("restart httpd"){
+        sh "ssh ec2-user@${ENV}      sudo systemctl restart httpd"
     }
+        
 }
