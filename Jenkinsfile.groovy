90f7f9cbe159a4dbe8092e5df9c876083fff2d68
daudmu21 <daudmuhudin@gmail.com>
2019-05-02 20:05:28 -0500

    add webserver

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,4 +2,16 @@ node{
     stage("Pull Repo"){
         git 'git@github.com:daudmu21/cool_website.git'
     }
+    stage("Webserver Install"){
+        sh "ssh ec2-user@54.171.239.132  sudo yum install httpd -y"
+    }
+    stage("Index file"){
+        sh "scp index.html  ec2-user@54.171.239.132:/tmp"
+    }
+    stage("Move Index"){
+        sh 'ssh ec2-user@54.171.239.132   "sudo mv /tmp/index.html /var/www/html/index.html"'
+    }
+    stage("Start HTTPD"){
+        sh "ssh ec2-user@54.171.239.132   sudo systemctl restart httpd"
+    }
 }
